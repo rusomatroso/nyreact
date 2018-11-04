620bc6ad1f1ac639e37b6e9dbeb103d97bf07562
rusomatroso <evgeny23kl@gmail.com>
2018-11-03 22:31:09 -0400

    still trying heroku to work...

---
+++ client/build/precache-manifest.c4171d310a06a7fc0e07c7bf125c4d4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b99b6b6d871f3f6c307",
+    "url": "/static/js/main.7b99b6b6.chunk.js"
+  },
+  {
+    "revision": "d0bcf904a758f3a03a7a",
+    "url": "/static/js/1.d0bcf904.chunk.js"
+  },
+  {
+    "revision": "7b99b6b6d871f3f6c307",
+    "url": "/static/css/main.9142b727.chunk.css"
+  },
+  {
+    "revision": "3b10b9636ab0ba31dcc508ab95baf94f",
+    "url": "/index.html"
+  }
+];
