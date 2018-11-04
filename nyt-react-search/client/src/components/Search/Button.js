f21f6f9e9a61f070a8c3cf418dee1f2535b4d3aa
rusomatroso <evgeny23kl@gmail.com>
2018-11-03 22:56:48 -0400

    still trying heroku to work........

--- nyt-react-search/client/src/components/Search/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = (props) => {
-  return(
-    <button onClick={props.handleButtonClick} className="btn btn-danger btn-lg">Search</button>
-  )
-};
-
-export default Button;
